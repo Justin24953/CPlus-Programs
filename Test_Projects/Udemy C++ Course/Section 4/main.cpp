8596b8559762f3dc549793081fe2fdbcfa706976
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-10-14 21:01:24 -0400

    File Restructure

---
+++ Test_Projects/Udemy C++ Course/Section 4/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+    int favorite_number;
+    
+    std::cout << "Enter Your Favorite Number Between 1 and 100:";
+    
+    std::cin  >>  favorite_number;
+    
+    std::cout << " Good Job!: " << std::endl;
+    
+    return 0;
+}
