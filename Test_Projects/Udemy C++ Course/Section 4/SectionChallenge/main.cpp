8d1b8badbc28ceb8c7e1f04a4deee88168e04358
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-10-18 20:53:28 -0400

    C++ Challenge. Need more work on the comment line

---
+++ Test_Projects/Udemy C++ Course/Section 4/SectionChallenge/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main () 
+{
+    int favoriteNumber;
+    cout << " Enter Favorite # Between 1 - 100!" ;
+    cin >> favoriteNumber;
+    cout <<  "Wow " favoriteNumber"is my favorite number";
+    cout << "No Really it is my favorite number " << endl;
+}
