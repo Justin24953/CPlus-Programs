57ea58418408a1a59c8643da8734351abba1d144
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-10-18 21:33:44 -0400

    Finished Section Challenge. Before Looking at Answer Key

--- Test_Projects/Udemy C++ Course/Section 4/SectionChallenge/main.cpp
+++ Test_Projects/Udemy C++ Course/Section 4/SectionChallenge/main.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 
-
 int main () 
 {
     int favoriteNumber;
     cout << " Enter Favorite # Between 1 - 100!" ;
     cin >> favoriteNumber;
-    cout <<  "Wow " favoriteNumber"is my favorite number";
-    cout << "No Really it is my favorite number " << endl;
+    cout << "Wow "; cout << favoriteNumber; cout << " is my favorite as well." << endl;
+    // ^ There's no space between favotireNumber and String (FIXED!)
+
+    cout << "No Really "; cout << favoriteNumber; cout <<" is my favorite number " << endl;
+    // There has to be a faster way of me doing this.....
+    return 0;
+    // Figure Out Why I return 0 again!
 }
