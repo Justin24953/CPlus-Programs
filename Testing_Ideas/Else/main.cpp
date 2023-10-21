972956702159db8ed0e266c5f9640773d69485cf
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-10-20 20:42:24 -0400

    Testing If and Else statements in C++

---
+++ Testing_Ideas/Else/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int age;
+     cout << "Enter Your Age" << endl;
+     cin >>  age;
+     if (age >= 21)
+ { cout << " You're Able To Order Your Drink" << endl;
+ } else 
+     { cout << "You're Not Of Age To Order From Us" << endl;
+     }
+     return 0;
+}
+
