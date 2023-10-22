b5ee2920d8332dbff15088d45ba056eeae04b165
Justin24953 <121590009+Justin24953@users.noreply.github.com>
2023-10-22 19:39:02 -0400

    Basic cout and cin practice

---
+++ Test_Projects/Udemy C++ Course/Section 5/Basic-Cin-Cout/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+     // cout << " Hello World" << endl;
+     // cout << " Hi" << " World" << endl;
+    
+   // int num1;
+    
+ //   int num2;
+    
+    double num3;
+    
+   /*  cout << "Enter Your First Interger" << endl;
+    
+    cin >> num1;
+    
+    cout << "Enter Your Second Interger" << endl;
+    
+    cin >> num2;
+    
+    cout << " You've Entered " << num1 << " & "  << num2 << endl;
+    
+    cout << ; */
+    
+   /* cout << "Enter 2 intergers seperated with a space: " << endl;
+    
+    cin >> num1 >> num2;
+    
+    cout <<  " You've Entered " <<  num1 << " & " << num2 << endl; */
+    
+        cout << "Enter a Double" << endl;
+        cin >> num3;
+        cout << "You entered " << num3 << endl; 
+    
+     return 0;
+}
+
